75ba063f0845923e875c9e5c62915195c249bcc0
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-28 15:00:49 +0500

    user & dealer models created

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+from core.base_models import BaseModel
+
+
+class UserProfile(BaseModel):
+
+    USER_CHOICES = [
+        ('oddiy', 'Oddiy'),
+        ('diler', 'Diler')
+    ]
+
+    user_type = models.CharField(max_length=6, choices=USER_CHOICES)
+    phone = models.CharField(max_length=16)
+    avatar = models.ImageField(upload_to='avatar/', null=True, blank=True)
+    location = models.CharField(max_length=50)
+    rating = models.FloatField()
+
+class Dealer(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+    company_name = models.CharField(max_length=50)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='logo/', null=True, blank=True)
+    website = models.URLField()
+    address = models.TextField()
+    is_verified = models.BooleanField()
+    rating = models.FloatField()
